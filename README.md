eef7fc8491a7e90aa0ead1f7b385ed97735020ca
fuweihu.fwh <fuweihu.fwh@bytedance.com>
2022-05-15 16:23:46 +0800

    done: send_msg of producer 3 way

--- README.md
+++ README.md
@@ -1,32 +1,35 @@
+# rmq demo
 
+本地docker 启动
 
-### demo
+### deploy
 
-##### deploy
+* [本地调试部署](./deploy/local/README.md)
 
-* 本地调试部署
+### demos
 
-##### code
-
-* producer
-    * 同步发送消息
-    * 异步
-    * 单向
+* [发送消息](./demos/send_msg/)
+    * [同步发送消息](./demos/send_msg/src/main/java/com/example/demo/SyncSendMessageProducer.java)
+    * [异步](./demos/send_msg/src/main/java/com/example/demo/ASyncSendMessageProducer.java)
+    * [单向](./demos/send_msg/src/main/java/com/example/demo/OneWaySendMessageProducer.java)
 * 顺序消息
     * 生产
+        * 全局有序
+        * 分区顺序
     * 消费
 * 延迟消息
+* 消息可靠性
+    * 发送不丢失： 消息重投
+    * 存储不丢失 这里不管
+    * 消费不丢失： 消息重试
 * 死信队列
+* 回溯消费
 * 批量消息
     * 发送
     * 消费
 * 过滤消息
-* 事务
+* 事务消息
 * 日志格式
-
-### .
-
-本地docker 启动
 
 ### ref
 
---
+++ demos/send_msg/src/main/java/com/example/demo/OneWaySendMessageProducer.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
+
+public class OneWaySendMessageProducer {
+
+    public static void main(String[] args) throws MQClientException, RemotingException, InterruptedException, UnsupportedEncodingException {
+        // 实例化消息生产者Producer
+        DefaultMQProducer producer = new DefaultMQProducer("send_msg_oneway");
+        // 设置NameServer的地址
+        producer.setNamesrvAddr("localhost:9876");
+        // 启动Producer实例
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            // 创建消息，并指定Topic，Tag和消息体
+            Message msg = new Message("send_msg_oneway" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            // 发送单向消息，没有任何返回结果
+            producer.sendOneway(msg);
+
+        }
+        // 如果不再发送消息，关闭Producer实例。
+        producer.shutdown();
+    }
+}
